d8f935eea18d3bb8ac6b83d0f6c9ff1b772dddbb
Peanuts <849421294godw@gmail.com>
2020-06-29 18:25:09 +0900

    20200629 1725

---
+++ MySQL/185. Department Top Three Salaries.sql
@@ -0,0 +1,73 @@
+--
+-- @lc app=leetcode.cn id=185 lang=mysql
+--
+-- [185] 部门工资前三高的所有员工
+--
+-- https://leetcode-cn.com/problems/department-top-three-salaries/description/
+--
+-- database
+-- Hard (43.90%)
+-- Likes:    330
+-- Dislikes: 0
+-- Total Accepted:    28K
+-- Total Submissions: 63.8K
+-- Testcase Example:  '{"headers": {"Employee": ["Id", "Name", "Salary", "DepartmentId"], "Department": ["Id", "Name"]}, "rows": {"Employee": [[1, "Joe", 85000, 1], [2, "Henry", 80000, 2], [3, "Sam", 60000, 2], [4, "Max", 90000, 1], [5, "Janet", 69000, 1], [6, "Randy", 85000, 1], [7, "Will", 70000, 1]], "Department": [[1, "IT"], [2, "Sales"]]}}'
+--
+-- Employee 表包含所有员工信息，每个员工有其对应的工号 Id，姓名 Name，工资 Salary 和部门编号 DepartmentId 。
+-- 
+-- +----+-------+--------+--------------+
+-- | Id | Name  | Salary | DepartmentId |
+-- +----+-------+--------+--------------+
+-- | 1  | Joe   | 85000  | 1            |
+-- | 2  | Henry | 80000  | 2            |
+-- | 3  | Sam   | 60000  | 2            |
+-- | 4  | Max   | 90000  | 1            |
+-- | 5  | Janet | 69000  | 1            |
+-- | 6  | Randy | 85000  | 1            |
+-- | 7  | Will  | 70000  | 1            |
+-- +----+-------+--------+--------------+
+-- 
+-- Department 表包含公司所有部门的信息。
+-- 
+-- +----+----------+
+-- | Id | Name     |
+-- +----+----------+
+-- | 1  | IT       |
+-- | 2  | Sales    |
+-- +----+----------+
+-- 
+-- 编写一个 SQL 查询，找出每个部门获得前三高工资的所有员工。例如，根据上述给定的表，查询结果应返回：
+-- 
+-- +------------+----------+--------+
+-- | Department | Employee | Salary |
+-- +------------+----------+--------+
+-- | IT         | Max      | 90000  |
+-- | IT         | Randy    | 85000  |
+-- | IT         | Joe      | 85000  |
+-- | IT         | Will     | 70000  |
+-- | Sales      | Henry    | 80000  |
+-- | Sales      | Sam      | 60000  |
+-- +------------+----------+--------+
+-- 
+-- 解释：
+-- 
+-- IT 部门中，Max 获得了最高的工资，Randy 和 Joe 都拿到了第二高的工资，Will
+-- 的工资排第三。销售部门（Sales）只有两名员工，Henry 的工资最高，Sam 的工资排第二。
+-- 
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT  d.Name as Department, e1.Name as Employee,  e1.Salary FROM Employee e1 JOIN Department d ON e1.DepartmentId = d.Id 
+WHERE
+    3 > (
+        SELECT
+            count(DISTINCT e2.Salary)
+        FROM
+            Employee e2
+        WHERE
+            e1.Salary < e2.Salary AND e1.DepartmentId = e2.DepartmentId
+    )
+
+-- @lc code=end
+
