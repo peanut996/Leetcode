a2f03297a1d4d2822b3ef096ad11764738977801
Peanuts <849421294godw@gmail.com>
2020-10-13 17:55:37 +0800

    20201013 1755

--- Java/Solution.java
+++ Java/Solution.java
@@ -1,12 +1,19 @@
 package Java;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
 
 class Solution {
-    public String minNumber(int[] nums) {
-        return IntStream.of(nums).mapToObj(String::valueOf).sorted((o1,o2) -> (o1+o2).compareTo(o2+o1)).collect(Collectors.joining());
+    public static void main(String[] args) {
+        System.out.println(helper(16));
+        ArrayList<Integer> arrays = new ArrayList<Integer>();
+        System.out.println(arrays);
+    }
+    public static int m = 3;
+    public  static int helper(int n){
+        int res = 0;
+        for(int i=2;i<n+1;i++){
+            res = (res+m)%i;
+        }
+        return res+1;
     }
 }
-/*
-*/
